d61c03e0312adfe88a7899f6b089e14d3f02b111
goruav934131262-debug <goruav934131262@gmail.com>
2026-09-02 16:16:05 +0530

    new PR added

---
+++ Quote Generator 2/script.js
@@ -0,0 +1,24 @@
+const API_KEY="http://api.quotable.io/random";
+
+const quotebox=document.querySelector(".quote");
+const authbox=document.querySelector(".auth");
+const btn=document.querySelector(".new_btn")
+
+
+
+async function getdata(){
+    const response= await fetch(API_KEY);
+    let data=await response.json();
+    console.log(data.content)
+    quotebox.innerHTML=data.content;
+    authbox.innerHTML=data.author;
+}
+
+function tweet(){
+    window.open(`https://x.com/intent/tweet?text=${quotebox.innerHTML}`)
+}
+
+
+btn.addEventListener("click",()=>{
+   getdata();
+})
